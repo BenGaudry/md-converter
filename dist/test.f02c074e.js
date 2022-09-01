abb522e849d8442b9b50b8735614f82f529257e3
BenGaudry <totoro42360@gmail.com>
2022-09-01 17:12:36 +0200

    Ready for github pages

--- dist/test.f02c074e.js
+++ dist/test.f02c074e.js
@@ -4,24 +4,24 @@ function convert(str) {
     // On vide le renderer de la page avant d'insérer le nouveau texte dedans
     renderer.innerHTML = "";
     // Split le texte par ligne
-    let ligns = str.split("\n");
-    var len = ligns.length;
+    let lines = str.split("\n");
+    var len = lines.length;
     for(let i = 0; i <= len; i++){
-        let lign = ligns[i];
-        var newlign = "";
-        // let newlign = lign.replace(/([^#][a-zA-Z0-9\s]+)/g, '$1');
-        if (lign.match(/(^# [a-zA-Z0-9\s]+)/g)) {
-            newlign = lign.replace("#", "");
-            newlign = "<h1>" + newlign + "</h1>";
-        } else if (lign.match(/(^## [a-zA-Z0-9\s]+)/g)) {
-            newlign = lign.replaceAll("#", "");
-            newlign = "<h2>" + newlign + "</h2>";
-        } else if (lign.match(/(^### [a-zA-Z0-9\s]+)/g)) {
-            newlign = lign.replaceAll("#", "");
-            newlign = "<h3>" + newlign + "</h3>";
-        } else newlign = lign;
-        let fullnewlign = '<p class="renderer-lign">' + newlign + "</p>";
-        renderer.innerHTML += fullnewlign;
+        let line = lines[i];
+        var newline = "";
+        // let newline = line.replace(/([^#][a-zA-Z0-9\s]+)/g, '$1');
+        if (line.match(/(^# [a-zA-Z0-9\s]+)/g)) {
+            newline = line.replace("#", "");
+            newline = "<h1>" + newline + "</h1>";
+        } else if (line.match(/(^## [a-zA-Z0-9\s]+)/g)) {
+            newline = line.replaceAll("#", "");
+            newline = "<h2>" + newline + "</h2>";
+        } else if (line.match(/(^### [a-zA-Z0-9\s]+)/g)) {
+            newline = line.replaceAll("#", "");
+            newline = "<h3>" + newline + "</h3>";
+        } else newline = line;
+        let fullnewline = '<p class="renderer-line">' + newline + "</p>";
+        renderer.innerHTML += fullnewline;
     }
 }
 textarea.addEventListener("input", ()=>{
